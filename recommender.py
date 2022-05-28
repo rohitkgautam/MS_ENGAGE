69d8b9e9086f1f10fa8c5541ec9aa8f360500e33
rohitkgautam <106329092+rohitkgautam@users.noreply.github.com>
2022-05-29 02:12:55 +0530

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -13,7 +13,7 @@ with open(r'titles.json', 'r+', encoding='utf-8') as f:
 
 # Create dummy target variable for the KNN Classifier
 target = [0 for item in movie_titles]
-# Test point with movie data < Change the movie with macro provided in test_movies.py or add your own >
+# Test point with movie data < Change the movie with macro provided in test_movies.py >
 test_point = test_movies.AVENGERS_INFINITY_WAR
 # Instantiate object for the Classifier
 model = KNearestNeighbours(data, target, test_point, k=10)
@@ -22,6 +22,6 @@ model.fit()
 
 # Distances to most distant movie
 max_dist = sorted(model.distances, key=itemgetter(0))[-1]
-# Print list of 10 recommendations < Change value of k for a different number >
+# Print list of 5 recommendations < Change value of k for a different number >
 for i in model.indices:
     print(movie_titles[i][0] + ' ----> ' + str(model.distances[i][0]))
